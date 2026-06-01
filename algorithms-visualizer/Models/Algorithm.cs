550d53aebe6273af1a1bd28bbcf761c264919d2e
SorryTea <yarosalvkhomko@gmail.com>
2026-06-01 17:26:49 +0200

    feat: add Category, Algorithm and ExecutionLog entity models

---
+++ algorithms-visualizer/Models/Algorithm.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace algorithms_visualizer.Models
+{
+    public class Algorithm
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string DisplayName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(2000)]
+        public string PseudoCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string TimeComplexity { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)]
+        public string SpaceComplexity { get; set; } = string.Empty;
+
+        public bool IsVisible { get; set; } = true;
+
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
