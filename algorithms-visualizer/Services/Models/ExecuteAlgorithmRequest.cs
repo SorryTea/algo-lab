3a3d80c9dd67a49c124660b205ab21aef394c155
SorryTea <yarosalvkhomko@gmail.com>
2026-06-02 16:45:17 +0200

    feat: add POST /execute endpoint with ExecutionLog tracking

---
+++ algorithms-visualizer/Services/Models/ExecuteAlgorithmRequest.cs
@@ -0,0 +1,7 @@
+namespace algorithms_visualizer.Services.Models;
+
+public class ExecuteAlgorithmRequest
+{
+    public string AlgorithmName { get; set; } = string.Empty;
+    public int[] InputData { get; set; } = [];
+}
