579da62081c3c20c789bd92132d550137a417811
SorryTea <yarosalvkhomko@gmail.com>
2026-06-03 13:25:18 +0200

    feat: configure ASP.NET Identity with login UI

--- algorithms-visualizer/Program.cs
+++ algorithms-visualizer/Program.cs
@@ -1,12 +1,26 @@
 using algorithms_visualizer.Data;
 using algorithms_visualizer.Services;
 using algorithms_visualizer.Services.Sorting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(options =>
@@ -60,6 +74,7 @@ if (app.Environment.IsDevelopment())
 app.UseCors("DevPolicy");
 
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 
@@ -67,5 +82,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+app.MapRazorPages();
 
 app.Run();
