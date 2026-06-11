4043f211ab270ed67cc1c4ce2479c9be26b34ec8
SkyWalker0-0 <vladimir18012007@gmail.com>
2026-06-11 11:32:31 +0200

    feat: add page transition animations (#27)
    Dodaje płynne animacje przejść między stronami z użyciem biblioteki Motion (dawniej Framer Motion).
    
    - motion: nowa zależność, import z motion/react, zgodna z React 19.
    - PageTransition.jsx: wrapper animujący wejście i wyjście strony (delikatne przesunięcie w pionie z zanikaniem).
    - Layout.jsx: przejście z układu opartego na Outlet na przyjmowanie children, dzięki czemu nawigacja i stopka pozostają poza obszarem animacji.
    - App.jsx: trasy owinięte w AnimatePresence z mode="wait" i kluczem po ścieżce, każda strona w PageTransition.
    
    Animowana jest tylko treść strony, nawigacja i stopka nie migają przy zmianie trasy.

--- algorithms-visualizer/frontend/src/App.jsx
+++ algorithms-visualizer/frontend/src/App.jsx
@@ -1,26 +1,31 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
+import { AnimatePresence } from "motion/react";
 import Layout from "./components/layout/Layout";
+import PageTransition from "./components/PageTransition";
 import Home from "./pages/Home";
 import Algorithms from "./pages/Algorithms";
 import Visualizer from "./pages/Visualizer";
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
 import Compare from "./pages/Compare";
+import About from "./pages/About";
 import Faq from "./pages/Faq";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="algorithms" element={<Algorithms />} />
-        <Route path="compare" element={<Compare />} />
-        <Route path="/algorithms/:id" element={<Visualizer />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="faq" element={<Faq />} />
-      </Route>
-    </Routes>
+    <Layout>
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route index element={<PageTransition><Home /></PageTransition>} />
+          <Route path="/algorithms" element={<PageTransition><Algorithms /></PageTransition>} />
+          <Route path="/algorithms/:id" element={<PageTransition><Visualizer /></PageTransition>} />
+          <Route path="/compare" element={<PageTransition><Compare /></PageTransition>} />
+          <Route path="/about" element={<PageTransition><About /></PageTransition>} />
+          <Route path="/faq" element={<PageTransition><Faq /></PageTransition>} />
+          <Route path="*" element={<PageTransition><NotFound /></PageTransition>} />
+        </Routes>
+      </AnimatePresence>
+    </Layout>
   );
 }
 
